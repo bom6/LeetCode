fbcec1a4b123ee4e9078c0b841e6b217b46f4cce
Brenda Olivia Martis <brendaoliviamartis@gmail.com>
2020-05-31 21:43:17 -0700

    First bad version

--- BinarySearch/278.first-bad-version.java
+++ BinarySearch/278.first-bad-version.java
@@ -3,6 +3,15 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+        int left = 0, right = n;
+        while(left < right) {
+            int middle = left+(right-left)/2;
+            if(isBadVersion(middle)) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return left;
     }
 }
