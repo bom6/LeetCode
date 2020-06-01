987027ffbcd08d00d55f9ed21ffd9f0892a8dafc
Brenda Olivia Martis <brendaoliviamartis@gmail.com>
2020-06-01 16:32:22 -0700

    Add and search word - DS design

---
+++ Design/211.add-and-search-word-data-structure-design.java
@@ -0,0 +1,61 @@
+class WordDictionary {
+
+    class TrieNode {
+        Map<Character, TrieNode> children;
+        boolean end;
+        TrieNode(){
+            children = new HashMap<Character, TrieNode>();
+        }
+    }
+
+    TrieNode root;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();    
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++) {
+            Character ch = word.charAt(i);
+            if(node.containsKey(ch)) {
+                node = node.children.get(ch);
+            } else {
+                TrieNode newNode = new TrieNode();
+                node.children.put(ch, newNode);
+            }
+        }
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return search(word, root);
+    }
+
+    private boolean search(String word, TrieNode node) {
+        for(int i = 0; i < word.length(); i++) {
+            Character ch = word.charAt(i);
+            if(!node.containsKey(ch)) {
+                if(ch == '.') {
+                    for (Character key : node.children.keySet()) {
+                        if (search(word.substring(i+1, word.length()), node.children.get(key))) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            node = node.children.get(ch);
+        }
+        return node.endWord;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
