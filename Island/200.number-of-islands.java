124ac91f47ac8dc0c9a18dd48436dcdd673df2e4
Brenda Olivia Martis <brendaoliviamartis@gmail.com>
2020-06-01 23:27:11 -0700

    Number of islands

---
+++ Island/200.number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0)
+            return 0;
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    merge(grid, i, j);
+                    count = count+1;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void merge(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j >= grid[0].length || j < 0 || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        merge(grid, i-1, j);
+        merge(grid, i, j-1);
+        merge(grid, i+1, j);
+        merge(grid, i, j+1);
+    }
+}
