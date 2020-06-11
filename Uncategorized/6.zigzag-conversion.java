7d6423d5dedf0c0f7595a5c4c4e9e706ad2f8389
bom6 <brendaoliviamartis@gmail.com>
2020-06-11 13:08:19 -0700

    Zigzag conversion

---
+++ Uncategorized/6.zigzag-conversion.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for(int i = 0; i <numRows; i++) {
+            sb[i] = new StringBuilder();
+        }
+        for(int i = 0; i < s.length(); i++) {
+            
+        }
+        return sb.toString();
+    }
+}
