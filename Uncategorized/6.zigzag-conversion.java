f1c6a21fa62e17fd215a7433949414f93ac863b7
bom6 <brendaoliviamartis@gmail.com>
2020-06-11 14:39:41 -0700

    Update zigzag  conversion

--- Uncategorized/6.zigzag-conversion.java
+++ Uncategorized/6.zigzag-conversion.java
@@ -4,9 +4,24 @@ class Solution {
         for(int i = 0; i <numRows; i++) {
             sb[i] = new StringBuilder();
         }
+        int index = 0;
+        int inc = -1;
+
+                
+        if(numRows <= 1)
+            return s;
+        
         for(int i = 0; i < s.length(); i++) {
-            
+            sb[index].append(s.charAt(i));
+            if(index == 0 || index == numRows-1) {
+                inc = inc * -1;
+            }
+            index = index+inc;
         }
-        return sb.toString();
+
+        for(int i = 1; i < sb.length; i++) {
+            sb[0].append(sb[i]);
+        }
+        return sb[0].toString();
     }
 }
